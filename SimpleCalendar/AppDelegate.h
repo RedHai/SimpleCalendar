b244b70b1483952b0d8fef00ff2004de8cf59a52
Hiway Hong <hiwayhong@gmail.com>
2013-07-27 00:33:11 +0800

    Initial Commit

---
+++ SimpleCalendar/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SimpleCalendar
+//
+//  Created by hiway on 7/27/13.
+//  Copyright (c) 2013 hiway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SimpleCalendar/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SimpleCalendar
+//
+//  Created by hiway on 7/27/13.
+//  Copyright (c) 2013 hiway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
